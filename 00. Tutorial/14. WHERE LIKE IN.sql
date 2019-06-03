782793c2d7cd973c331f0e57647a6b397bf338ec
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 22:46:56 -0400

    Create 14. WHERE LIKE IN.sql

---
+++ 00. Tutorial/14. WHERE LIKE IN.sql
@@ -0,0 +1,10 @@
+# Filtering data
+# world.db
+
+SELECT * FROM Country;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 OR Population IS NULL ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 AND Continent = 'Oceania' ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Name LIKE '%island%' ORDER BY Name;
+SELECT Name, Continent, Population FROM Country WHERE Continent IN ( 'Europe', 'Asia' ) ORDER BY Name;
+
