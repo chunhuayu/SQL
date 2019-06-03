85972178fd96667c494105da86990e2adeac5f8f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 23:41:10 -0400

    Update 15. ORDER BY.sql

--- 00. Tutorial/15. ORDER BY.sql
+++ 00. Tutorial/15. ORDER BY.sql
@@ -1,9 +1,12 @@
-# ORDER BY
+# ORDER BY: to sort the result of the queries
 # world.db
+# default is ascending
 
 SELECT Name FROM Country;
-SELECT Name FROM Country ORDER BY Name;
-SELECT Name FROM Country ORDER BY Name DESC;
-SELECT Name FROM Country ORDER BY Name ASC;
-SELECT Name, Continent FROM Country ORDER BY Continent, Name;
+SELECT Name FROM Country ORDER BY Name;              # they are in alphabetical oreder according to ASCII character set
+SELECT Name FROM Country ORDER BY Name DESC;         # the order is by descending rather than ascending
+SELECT Name FROM Country ORDER BY Name ASC;          # default is ascending
+SELECT Name, Continent FROM Country ORDER BY Continent, Name;  
+#  the rows is sorted by ascending Continent first, and within the Continent the order is ascending by Name   
 SELECT Name, Continent, Region FROM Country ORDER BY Continent DESC, Region, Name;
+# the order is descending by Continent, and then ascending by Region, followed by ascending Name.
