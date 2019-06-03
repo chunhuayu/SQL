8292b7869ad2fb221474cbd897f3a0844375f8b9
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 20:31:51 -0400

    Create 07. CREATE TABLE.sql

---
+++ 00. Tutorial/07. CREATE TABLE.sql
@@ -0,0 +1,14 @@
+# CREATE TABLE
+# :memory:
+# assign observations
+
+CREATE TABLE test (
+  a INTEGER,
+  b TEXT
+);
+
+INSERT INTO test VALUES ( 1, 'a' );
+INSERT INTO test VALUES ( 2, 'b' );
+INSERT INTO test VALUES ( 3, 'c' );
+SELECT * FROM test;
+
