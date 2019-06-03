aa62e3b14f222984a2baf96a224419a89103ae05
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 20:19:30 -0400

    Create 04.  INSERT.sql

---
+++ 00. Tutorial/04.  INSERT.sql
@@ -0,0 +1,6 @@
+# 07 Inserting Data
+# test.db
+
+SELECT * FROM customer;
+INSERT INTO customer (name, address, city, state, zip) VALUES ('Fred Flintstone', '123 Cobblestone Way', 'Bedrock', 'CA', '91234');
+INSERT INTO customer (name, city, state) VALUES ('Jimi Hendrix', 'Renton', 'WA');
