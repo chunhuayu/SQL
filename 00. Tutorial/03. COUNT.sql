0bd91b4174a3d12adf08f6590b2ea169f80c3e78
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 20:22:16 -0400

    Update 03. COUNT.sql

--- 00. Tutorial/03. COUNT.sql
+++ 00. Tutorial/03. COUNT.sql
@@ -5,3 +5,8 @@ SELECT COUNT(*) FROM Country;
 SELECT COUNT(*) FROM Country WHERE Population > 1000000;
 SELECT COUNT(*) FROM Country WHERE Population > 100000000;
 SELECT COUNT(*) FROM Country WHERE Population > 100000000 AND Continent = 'Europe' ;
+
+
+SELECT COUNT(*) FROM Country;
+SELECT COUNT(LifeExpectancy) FROM Country; 
+# display the numbers of rows which the value is non-zero value
