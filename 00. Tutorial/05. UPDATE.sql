cf28c2b7023f25d3550e4e8e44cefe1579da887a
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 20:24:12 -0400

    Create 05. UPDATE.sql

---
+++ 00. Tutorial/05. UPDATE.sql
@@ -0,0 +1,7 @@
+# 08 Updating Data
+# test.db
+
+SELECT * FROM customer;
+UPDATE customer SET address = '123 Music Avenue', zip = '98056' WHERE id = 5;
+UPDATE customer SET address = '2603 S Washington St', zip = '98056' WHERE id = 5;
+UPDATE customer SET address = NULL, zip = NULL WHERE id = 5;
