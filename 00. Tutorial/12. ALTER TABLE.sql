71601d41e12953e522b8893b759567394bc05d03
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 21:16:26 -0400

    Create 12. ALTER TABLE.sql

---
+++ 00. Tutorial/12. ALTER TABLE.sql
@@ -0,0 +1,14 @@
+# ALTER TABLE
+# test.db
+
+CREATE TABLE test ( a TEXT, b TEXT, c TEXT );
+INSERT INTO test VALUES ( 'one', 'two', 'three');
+INSERT INTO test VALUES ( 'two', 'three', 'four');
+INSERT INTO test VALUES ( 'three', 'four', 'five');
+SELECT * FROM test;
+
+ALTER TABLE test ADD d TEXT;
+ALTER TABLE test ADD e TEXT DEFAULT 'panda';
+
+DROP TABLE test;
+
