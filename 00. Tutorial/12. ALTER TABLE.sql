7b8dcdbb42a3dbaedf134ff21bf0ac5aaff0f554
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-02 21:40:07 -0400

    Update 12. ALTER TABLE.sql

--- 00. Tutorial/12. ALTER TABLE.sql
+++ 00. Tutorial/12. ALTER TABLE.sql
@@ -7,8 +7,8 @@ INSERT INTO test VALUES ( 'two', 'three', 'four');
 INSERT INTO test VALUES ( 'three', 'four', 'five');
 SELECT * FROM test;
 
-ALTER TABLE test ADD d TEXT;
-ALTER TABLE test ADD e TEXT DEFAULT 'panda';
+ALTER TABLE test ADD d TEXT;                    # add d column which is not assigned value, that is NULL
+ALTER TABLE test ADD e TEXT DEFAULT 'panda';    # add e column which has default value = 'panda'
 
-DROP TABLE test;
+DROP TABLE test;                                # delete table test
 
