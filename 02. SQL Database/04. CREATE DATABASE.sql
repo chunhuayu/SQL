9c5a0ff0fe90c7d3667461aa575fd41d72f275ac
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-05 02:32:34 -0400

    Create 04. CREATE DATABASE.sql

---
+++ 02. SQL Database/04. CREATE DATABASE.sql
@@ -0,0 +1,12 @@
+/*
+The SQL CREATE DATABASE Statement
+The CREATE DATABASE statement is used to create a new SQL database.
+
+Syntax
+CREATE DATABASE databasename;
+CREATE DATABASE Example
+The following SQL statement creates a database called "testDB":
+*/
+
+
+CREATE DATABASE testDB;
