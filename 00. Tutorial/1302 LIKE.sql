7a4f74653e520158d0c8697254d39b6d923100bb
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-05 14:31:24 -0400

    Update 1302 LIKE.sql

--- 00. Tutorial/1302 LIKE.sql
+++ 00. Tutorial/1302 LIKE.sql
@@ -22,11 +22,11 @@ Demo Database
 Below is a selection from the "Customers" table in the Northwind sample database:
 
 CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
-1 Alfreds Futterkiste	Maria Anders	Obere Str. 57	Berlin	12209	Germany
-2	Ana Trujillo Emparedados y helados	Ana Trujillo	Avda. de la Constitución 2222	México D.F.	05021	Mexico
-3	Antonio Moreno Taquería	Antonio Moreno	Mataderos 2312	México D.F.	05023	Mexico
+1 Alfreds Futterkiste  Maria Anders  Obere Str. 57	Berlin	12209	Germany
+2 Ana Trujillo Emparedados y helados Ana Trujillo Avda. de la Constitución 2222  México D.F.	05021	Mexico
+3 Antonio Moreno Taquería	Antonio Moreno	Mataderos 2312	 México D.F.	05023	Mexico
 4 Around the Horn	Thomas Hardy	120 Hanover Sq.	London	WA1 1DP	UK
-5	Berglunds snabbköp	Christina Berglund	Berguvsvägen 8	Luleå	S-958 22	Sweden
+5 Berglunds snabbköp	Christina Berglund	Berguvsvägen 8	Luleå	S-958 22	Sweden
 
 SQL LIKE Examples
 The following SQL statement selects all customers with a CustomerName starting with "a":
@@ -50,7 +50,8 @@ WHERE CustomerName LIKE '%or%';
 SELECT * FROM Customers
 WHERE CustomerName LIKE '_r%';
 
--- The following SQL statement selects all customers with a CustomerName that starts with "a" and are at least 3 characters in length:
+-- The following SQL statement selects all customers with a CustomerName that starts with "a" and are at least 3 characters 
+-- in length:
 
 SELECT * FROM Customers
 WHERE CustomerName LIKE 'a__%';
