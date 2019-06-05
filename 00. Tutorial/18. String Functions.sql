be40badb5d1a8b4c99d9b52e580353f32fa9766f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-04 22:42:12 -0400

    Update 18. String Functions.sql

--- 00. Tutorial/18. String Functions.sql
+++ 00. Tutorial/18. String Functions.sql
@@ -1,10 +1,22 @@
-# LENGTH
+# SELECT 'a literal SQL string;'  this is a standard SQL a literal string is represented by a series of characters enclosed
+# in shigle quote marks. in the orderer version of MySQL double quotes marks may be used instead. 
+# Note: If you need to use a single quote in your string you should use two single quotes to represent the one.
+# String handling tends to be platform specific in SQL. 
+# A simple thing like concatenating two strings together can be very different on different platforms. 
+# Standard SQL provides a concatenation operator.  SELECT 'This'||'&'||'that'
+# SQLite, Postgres and many other systems support this standard.  
+# MySQL uses a function. SELECT CONCAT ('this','&','that')
+# Microsoft SQL uses a non-standard operator.   SELECT 'This' + '&' + 'that'
+
+
+# LENGTH  : length function is not the standard SQL function 
 # world.db
 
 SELECT LENGTH('string');
 SELECT Name, LENGTH(Name) AS Len FROM City ORDER BY Len DESC;
 
-# SUBSTR
+
+# SUBSTR(released, 1, 4)-------1 represent the first letter , 4 represent the length of substr letter
 # album.db
 
 SELECT SUBSTR('this string', 6);
@@ -16,19 +28,19 @@ SELECT released,
   ORDER BY released
 ;
 
-# TRIM
+# TRIM, by default, trim the spaces
 # :memory:
 
 SELECT '   string   ';
-SELECT TRIM('   string   ');
+SELECT TRIM('   string   ');                   # remove space from the both sides of string that means the beginning and end
 SELECT TRIM('   string   string   ');
-SELECT LTRIM('   string   ');
-SELECT RTRIM('   string   ');
-SELECT '...string...';
-SELECT TRIM('...string...', '.');
+SELECT LTRIM('   string   ');                  # remove space from left side
+SELECT RTRIM('   string   ');                  # remove space from right side
+SELECT '...string...';          
+SELECT TRIM('...string...', '.');              # the second argument represents to remove '.' from the string
 
 
-# UPPER/LOWER
+# UPPER/LOWER       for normalizing the strings for further used
 # world.db
 
 SELECT 'StRiNg';
