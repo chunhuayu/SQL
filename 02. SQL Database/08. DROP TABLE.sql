821846d311d6de2d0f9cec50fcccdbf348c1578a
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-05 02:07:07 -0400

    Update and rename 00. Tutorial/08. DROP TABLE.sql to 02. SQL Database/08. DROP TABLE.sql

--- 02. SQL Database/08. DROP TABLE.sql
+++ 02. SQL Database/08. DROP TABLE.sql
@@ -1,7 +1,14 @@
-# DROP TABLE
-# :memory:
-# delete a table = DROP TABLE
-# delete a table with conditions = DROP TABLE IF ...
+/* 
+DROP TABLE
+:memory:
+
+Syntax:
+DROP TABLE table_name;
+
+  delete a table = DROP TABLE
+  delete a table with conditions = DROP TABLE IF ...
+
+*/
 
 CREATE TABLE test ( a TEXT, b TEXT );
 INSERT INTO test VALUES ( 'one', 'two' );
