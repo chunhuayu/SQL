4d612e6355d353a8aeb2852dfcc214becfa8a756
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-05 01:27:44 -0400

    Update 19. Numbers.sql

--- 00. Tutorial/19. Numbers.sql
+++ 00. Tutorial/19. Numbers.sql
@@ -8,24 +8,48 @@ SELECT A, B, A = B FROM
   ( SELECT 
     ( ( .1 + .2 ) * 10 ) as A,
     ( 1.0 + 2.0 ) as B
-  );
+  );                         # the output is A=3.0 and B =3.0 but the value of A=B is 0, that means, they are not equal.
+
+SELECT A, B, A = B FROM 
+  ( SELECT 
+    ((.2+0.2)*10) as A,
+    (1.0+3.0) as B
+  );                        # the output is 4.0, 4.0, 1
   
+SELECT A, B, A = B FROM 
+  ( SELECT 
+    ((.1+0.21)*10) as A,
+    (1.0+2.1) as B
+  );                       # the output is 3.1, 3.1, 1
+ 
+ 
 # This should be equivalent. .1 plus .2 is .3 times 10, is 3.0, and if I compare A to B as I do here, 
 # you'll notice that I get a false result, because they are not identical because by using decimals, 
 # I'm using the floating point engine, and floating point numbers sacrifice accuracy for precision. 
 # And this is why we use integers, or fixed precision, for financial calculations.
 
+SELECT A, B, A = B FROM 
+  ( SELECT 
+    ((.2+.8)*3) as A,
+    (1.0+2.0) as B
+  );                        # the output is A=3.0 and B =3.0, the value of A=B is 1
 
+
+SELECT A , B , A=B FROM 
+  ( SELECT 
+    ( ( 1 + 2 ) * 10 ) as A,
+    ( 10 + 20 ) as B
+;                           # the value of A=B is 1, that means, they are both integer 20, they are equal.
 
 # typeof : returns a string that indicates the datatype of the expression.
 # The only return values are: "null", "integer", "real", "text", or "blob". 
 # :memory:
 
-SELECT TYPEOF( 1 + 1 );
-SELECT TYPEOF( 1 + 1.0 );
-SELECT TYPEOF('panda');
-SELECT TYPEOF('panda' + 'koala');
-
+SELECT TYPEOF( 1 + 1 );                      # integer
+SELECT TYPEOF( 1 + 1.0 );                    # real
+SELECT TYPEOF('panda');                      # string
+SELECT TYPEOF('panda' + 'koala');            # output is integer. because 'panda'+'koala' =0
+ 
 # INTEGER division
 # :memory:
 
