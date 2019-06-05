fa36ee7a6971f913fceaa602c72eb2ed5d3a483f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-05 13:27:41 -0400

    Create 11. DROP VIEW.sql

---
+++ 02. SQL Database/11. DROP VIEW.sql
@@ -0,0 +1,11 @@
+/*
+SQL Dropping a View
+A view is deleted with the DROP VIEW command.
+
+SQL DROP VIEW Syntax
+DROP VIEW view_name;
+The following SQL drops the "Brazil Customers" view:
+*/
+
+Example
+DROP VIEW [Brazil Customers];
